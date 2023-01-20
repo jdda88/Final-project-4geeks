9434fff93c62d809bed10d77bc7aadc4823888aa
Juan David Diaz <jdda@Juans-MacBook-Pro.local>
2023-01-19 20:54:37 -0500

    footer and maping added

--- front end/src/components/jumbotron.jsx
+++ front end/src/components/jumbotron.jsx
@@ -1,12 +1,14 @@
 import "../style/styles.css";
+import { useNavigate } from "react-router";
 
 export const Jumbotron = () => {
+    const navigate = useNavigate()
   return (
     <div>
       <div className="jumbotron">
         <div className="banner">
           <h1 className="banner-title">type of product</h1>
-          <button className="banner-btn">Show all</button>
+          <button className="banner-btn" onClick={()=>{navigate("/products")}}>Show all</button>
         </div>
       </div>
 
--- front end/src/views/home.jsx
+++ front end/src/views/home.jsx
@@ -1,24 +1,34 @@
 import React from "react";
+import { Footer } from "../components/footer";
 import { Nav } from "../components/navbar";
-import { Jumbotron } from "../components/jumbotron"
+import { Jumbotron } from "../components/jumbotron";
 import { Card } from "../components/card";
 import { get_categories } from "../service/fakestore";
 import { useEffect } from "react";
+import { useState } from "react";
+
 
 export const Home = () => {
 
-    useEffect(() =>{
-        async function set() {
-            let categories = await get_categories()
-            console.log(categories)
-        }
-        set()
-    })
-    return (
-        <>
-            <Nav />
-            <Jumbotron />
-            <Card />
-        </>
-    )
-}
+  const [categorieName, setCategorieName] = useState();
+
+  useEffect(() => {
+    async function set() {
+      let categories = await get_categories();
+      console.log(categories);
+      setCategorieName(categories);
+    }
+    set();
+  }, []);
+  return (
+    <>
+      <Nav />
+      <Jumbotron />
+      {categorieName?.map((name, index) => {
+        console.log(name);
+        return <Card key={index} name={name} />;
+      })}
+      <Footer />
+    </>
+  );
+};
