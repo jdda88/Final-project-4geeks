cf01cf16583542c99e169ee348678135048f77ca
Juan David Diaz <jdda@Juans-MacBook-Pro.local>
2023-01-21 09:40:02 -0500

    worked on useparams

--- front end/src/views/allproducts.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Nav } from "../components/navbar";
-
-export const Products = () =>{
-    return(
-        <Nav/>
-    )
-}
---
+++ front end/src/views/products.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { useParams } from "react-router";
+import { Nav } from "../components/navbar";
+
+
+export const Products = () => {
+  return <Nav />;
+};
+
+// pasar categorias por use param
