8f6b5e35d3e76d1ac5be9469cd01df6e5e989693
Juan David Diaz <jdda@Juans-MacBook-Pro.local>
2023-01-24 20:43:57 -0500

    Side bar & categories & logo button updated and almost functional

--- front end/src/components/navbar.jsx
+++ front end/src/components/navbar.jsx
@@ -1,14 +1,30 @@
-import React from "react";
-import "../style/styles.css"
+import React, { useState } from "react";
+import { useNavigate } from "react-router";
+import "../style/styles.css";
+import { Sidebar } from "./sidebar";
 
 export const Nav = () => {
+  const navigate = useNavigate();
+  const [visible, setVisible] = useState(false);
+
   return (
     <nav className="navbar">
       <div className="navbar-center">
-        <span className="nav-icon">
-          <i className="fas fa-bars"></i>
+        <span className="nav-icon" onClick={() => setVisible(!visible)}>
+            {/* Z index =1 on the CSS */}
+          {visible ? <Sidebar /> : <i className="fas fa-bars"></i>}
         </span>
-        <h1 className="logo">E-commerce site</h1>
+        {visible}
+
+        <div
+          type="button"
+          className="Backtohome"
+          onClick={() => {
+            navigate("/");
+          }}
+        >
+          <h1 className="logo">E-commerce site</h1>
+        </div>
         <div className="cart-btn">
           <span className="nav-icon">
             <i className="fas fa-cart-plus"></i>
--- front end/src/layout.jsx
+++ front end/src/layout.jsx
@@ -2,8 +2,9 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "./views/home.jsx";
 import {  Specificproduct } from "./views/specificproduct";
-import { Products } from "./views/products.jsx";
-import { Categories } from "./views/allproducts.jsx";
+import { Products } from "./views/allproducts.jsx";
+import { Categories } from "./views/products-by-category.jsx";
+
 
 function Layout() {
   return (
@@ -14,12 +15,12 @@ function Layout() {
           <Route exact path="/products/:category" element={<Categories />} />
           <Route path="/" element={<Home />} />
           <Route exact path="/products/id/:id" element={<Specificproduct />} />
-
           <Route path="*" element={<h1>Not found!</h1>} />
         </Routes>
       </BrowserRouter>
     </div>
   );
 }
+
 
 export default Layout;
