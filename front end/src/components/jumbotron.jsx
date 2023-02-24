37a7a9f2ca1b8586e9bb74bf37493729047001ee
Rodrigo <rodricelis@gmail.com>
2023-02-24 00:05:46 -0500

    Title animation working

--- front end/src/components/jumbotron.jsx
+++ front end/src/components/jumbotron.jsx
@@ -1,13 +1,42 @@
-import React from "react";
+import React, { useRef, useEffect }from "react";
 import "../style/styles.css";
+import "../style/title.css";
 import { GoblalButton } from "./generalbutton";
 
 export const Jumbotron = () => {
+
+
+  const textWrapperRef = useRef(null);
+
+  useEffect(() => {
+    // Wrap every letter in a span
+var textWrapper = document.querySelector('.ml12');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml12 .letter',
+    translateX: [40,0],
+    translateZ: 0,
+    opacity: [0,1],
+    easing: "easeOutExpo",
+    duration: 1200,
+    delay: (el, i) => 500 + 30 * i
+  }).add({
+    targets: '.ml12 .letter',
+    translateX: [0,-30],
+    opacity: [1,0],
+    easing: "easeInExpo",
+    duration: 1100,
+    delay: (el, i) => 100 + 30 * i
+  });
+  }, []);
+
   return (
     <div>
       <div className="jumbotron">
         <div className="banner">
-          <h1 className="banner-title">View all products</h1>
+        <h1 className="ml12">WELCOME TO BSB</h1>
           <GoblalButton name={"Show All"} path={"/products"} />
         </div>
       </div>
